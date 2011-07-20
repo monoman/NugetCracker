87ab9b2a77431b95911b756f629a524e6d450254
Rafael Teixeira <monoman@gmail.com>
2011-07-20 16:58:16 -0300

    The empty program just to start

---
+++ NugetCracker/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NugetCracker
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("NugetCracker 0.1\n\nJust started see https://github.com/monoman/NugetCracker");
+		}
+	}
+}
---
+++ NugetCracker/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NugetCracker")]
+[assembly: AssemblyDescription("A builder for versioned nugets with a web of dependencies")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NugetCracker Team")]
+[assembly: AssemblyProduct("NugetCracker")]
+[assembly: AssemblyCopyright("Copyright © NugetCracker Team 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1654cc24-907b-48c5-9297-e6f1e7ed14c2")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
